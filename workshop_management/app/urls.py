fb6452b770e288ffdd6ccdf44fd38605fd28981a
haridinesh00 <haridinesh00@gmail.com>
2023-03-14 22:53:46 +0530

    added template

--- workshop_management/app/urls.py
+++ workshop_management/app/urls.py
@@ -4,4 +4,11 @@ from app import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    path('404', views.error_page, name="error_page"),
+    path('about', views.about, name="about"),
+    path('booking', views.booking, name="booking"),
+    path('contact', views.contact, name="contact"),
+    path('service', views.service, name="service"),
+    path('team', views.team, name="team"),
+    path('testimonial', views.testimonial, name="testimonial"),
 ]
--- workshop_management/app/views.py
+++ workshop_management/app/views.py
@@ -4,3 +4,31 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(requests):
     return render(requests, 'index.html')
+
+
+def error_page(requests):
+    return render(requests, '404.html')
+
+
+def about(requests):
+    return render(requests, 'about.html')
+
+
+def booking(requests):
+    return render(requests, 'booking.html')
+
+
+def contact(requests):
+    return render(requests, 'contact.html')
+
+
+def service(requests):
+    return render(requests, 'service.html')
+
+
+def team(requests):
+    return render(requests, 'team.html')
+
+
+def testimonial(requests):
+    return render(requests, 'testimonial.html')
